33021e17ec8659e1cd6774689d629725cd0c6f56
Yanick <yanick.servant@free.fr>
2016-11-16 15:26:07 +0100

    Display QuestionsListbySurvey

--- Site/src/main/webapp/app/entities/question/question.service.js
+++ Site/src/main/webapp/app/entities/question/question.service.js
@@ -2,9 +2,11 @@
     'use strict';
     angular
         .module('swhappyApp')
-        .factory('Question', Question);
+        .factory('Question', Question)
+        .factory('QuestionBySurvey', QuestionBySurvey)
 
     Question.$inject = ['$resource'];
+    QuestionBySurvey.$inject = ['$resource'];
 
     function Question ($resource) {
         var resourceUrl =  'api/questions/:id';
@@ -22,5 +24,21 @@
             },
             'update': { method:'PUT' }
         });
+    }
+    
+    function QuestionBySurvey($resource){
+    	var resourceUrl = 'api/questions/survey/:id';
+    	return $resource(resourceUrl, {}, {
+    		'get': {
+                method: 'GET',
+                isArray: true,
+                transformResponse: function (data) {
+                    if (data) {
+                        data = angular.fromJson(data);
+                    }
+                    return data;
+                }
+    		}
+    	})
     }
 })();
