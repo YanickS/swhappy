fb33cab5eca8ec7d660da79c8ca3c9a597d0b47a
Sparhx <sparhx@gmail.com>
2016-11-16 11:10:54 +0100

    Add questions api interaction

--- Mobile/project/swhappy_app/www/js/services.js
+++ Mobile/project/swhappy_app/www/js/services.js
@@ -1,9 +1,33 @@
 angular.module('starter.services', [])
 
-.factory('QuestionService', function($http){
-  return{
-    getAll: function(){
-        return $http.get("https://swhappy.herokuapp.com/api/questions");
-    }
-  }
-});
+.factory('QuestionFactory', ['$http', function($http) {
+
+    var urlBase = 'https://swhappy.herokuapp.com/api/questions';
+    var QuestionFactory = {};
+
+    QuestionFactory.getQuestions = function () {
+        return $http.get(urlBase);
+    };
+
+    QuestionFactory.getQuestion = function (id) {
+        return $http.get(urlBase + '/' + id);
+    };
+
+    QuestionFactory.insertQuestion = function (question) {
+        return $http.post(urlBase, question);
+    };
+
+    QuestionFactory.updateQuestion = function (question) {
+        return $http.put(urlBase + '/' + question.ID, question)
+    };
+
+    QuestionFactory.deleteQuestion = function (id) {
+        return $http.delete(urlBase + '/' + id);
+    };
+
+    QuestionFactory.getQuestionsOfSurvey = function (id) {
+        return $http.get(urlBase + '/survey' + '/' + id );
+    };
+
+    return QuestionFactory;
+}]);
